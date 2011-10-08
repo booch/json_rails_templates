f5a895cec8f350d065d18ec186ddbfa1db27d525
Craig Buchek <craig@boochtek.com>
2011-10-08 15:14:18 -0500

    Implement JsonRailsTemplates.to_json for simple literals (where JSON is equivalent to Ruby)

--- lib/json_rails_templates.rb
+++ lib/json_rails_templates.rb
@@ -1,4 +1,20 @@
 class JsonRailsTemplates
-  def initialize()
+  attr_accessor :template_text
+
+  def initialize(template_text)
+    self.template_text = template_text
+  end
+
+  def to_json
+    "{#{template_text.lines.collect{ |line| one_line_to_json(line) }.join(",\n")}}"
+  end
+
+private
+
+  def one_line_to_json(line)
+    left, right = line.split(':', 2)
+    left = left.strip
+    right = right.strip
+    "#{left}: #{right}"
   end
 end
--- spec/json_rails_templates_spec.rb
+++ spec/json_rails_templates_spec.rb
@@ -1,4 +1,33 @@
 require 'json_rails_templates'
 
 describe 'JsonRailsTemplates' do
+  describe '.to_json' do
+    subject { JsonRailsTemplates.new(template_text).to_json }
+    let(:template_text) { '' } # NOTE: We'll override this for each spec below.
+
+    context 'for a string literal' do
+      let(:template_text) { %(string_literal: 'this is a string') }
+      it { should == %({string_literal: 'this is a string'}) }
+    end
+
+    context 'for an integer literal' do
+      let(:template_text) { %(integer_literal: 123) }
+      it { should == %({integer_literal: 123}) }
+    end
+
+    context 'for a float literal' do
+      let(:template_text) { %(float_literal: 1.234) }
+      it { should == %({float_literal: 1.234}) }
+    end
+
+    context 'for a boolean literal' do
+      let(:template_text) { %(boolean_literal: true) }
+      it { should == %({boolean_literal: true}) }
+    end
+
+    context 'for multiple simple literals' do
+      let(:template_text) { %(boolean_literal: false\ninteger_literal: 1) }
+      it { should == %({boolean_literal: false,\ninteger_literal: 1}) }
+    end
+  end
 end
